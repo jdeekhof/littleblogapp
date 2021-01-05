62c1afbbce2fad486bd9e19e5559f79ccd71cb43
Jacob Eekhof <jdeekhof@gmail.com>
2021-01-05 18:12:21 -0500

    conditionalized the log in and out buttons and made the alerts prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
 	root "welcome#index"
+	devise_for :admins
+	resources :admins
 	resources "entries"
 	get "contact", to: "welcome#contact"
 end
